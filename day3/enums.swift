878e13bc44ecb61945985c7aac3bc33165586f4b
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-05 18:08:15 +0300

    Different ways of writing enums

---
+++ day3/enums.swift
@@ -0,0 +1,25 @@
+// Enums
+
+// First way
+
+// enum Weekday {
+//     case monday
+//     case tuesday
+//     case wednesday
+//     case thursday
+//     case friday
+// }
+
+// var day = Weekday.monday
+// day = Weekday.tuesday
+// day = Weekday.friday
+
+// Short way
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday
