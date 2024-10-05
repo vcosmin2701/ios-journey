cb97f7048cf775b9b981bd5060fa70c04adbfb4e
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-05 18:07:39 +0300

    How to specify the type of a dictionary

--- day3/dictionaries.swift
+++ day3/dictionaries.swift
@@ -6,3 +6,7 @@ let favCharacter = [
 
 // optionals
 print(favCharacter["name", default: "uNknown"], favCharacter["job", default: "uNknown"], favCharacter["location", default: "uNknown"])
+
+// we can set what type the key and the value can be 
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Jocker"
