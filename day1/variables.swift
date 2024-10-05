0fe33603e88f09f402a46454523853669466063e
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-05 13:40:23 +0300

    Basic variables knowledge

---
+++ day1/variables.swift
@@ -0,0 +1,11 @@
+// var - can be changed over time
+var greeting = "Hello, Cosmo"
+greeting = "Hello again, Cosmo"
+
+print(greeting)
+
+// constant - can't be changed
+let character = "Tom"
+// character = "Jerry" -> this will cause an error, because is a 'let' constant
+
+print(character)
