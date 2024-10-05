14265052c4435515d12239ebd5ff72650bf00315
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-05 15:21:02 +0300

    Basic Boolean informations

---
+++ day2/booleans.swift
@@ -0,0 +1,22 @@
+// Boolean -> true or false
+
+let isMultiple = 120.isMultiple(of: 3)
+print(isMultiple)
+
+// ! not operator
+
+var isAuthentificated = false
+
+isAuthentificated = !isAuthentificated
+print(isAuthentificated)
+
+isAuthentificated = !isAuthentificated
+print(isAuthentificated)
+
+// toggle() to flip bool value
+
+var gameOver = false
+print("Game Over: \(gameOver)")
+
+gameOver.toggle()
+print("Game Over: \(gameOver)")
