dc1714f7d54ef4d243b1da860c5b09df02efa2e0
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-05 17:56:43 +0300

    Introduction of Sets in Swift

---
+++ day3/sets.swift
@@ -0,0 +1,12 @@
+// Sets
+// Duplicate items can't be added, and items are not stored in a particular order.
+let people = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+
+print(people)
+
+// Insert values into a set
+var brands = Set<String>()
+brands.insert("Apple")
+brands.insert("Casio")
+
+print(brands)
