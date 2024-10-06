958732120a6ed6ce510b950b9388b1da7a2ded4c
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-06 15:52:31 +0300

    If else-if else example with enums

--- day5/conditionals.swift
+++ day5/conditionals.swift
@@ -19,3 +19,32 @@ if username.isEmpty {
 } else {
     print("Hello, \(username)")
 }
+
+// Multiple conditions
+
+let temp = 25
+if temp > 20{
+    print("Perfect time to go running outside")
+} else if temp > 15 && temp < 20 {
+    print("It's okay to go for a run, but grab some clothes")
+} else {
+    print ("Brrr, cold this time, but keep going")
+}
+
+// Enums example
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly")
+} else if transport == .bicycle {
+    print("Hope there's a bike path..")
+} else if transport == .car {
+    print("Time to get stuck in traffic")
+} else {
+    print("Scooter goes brrrr!!!!!!")
+}
